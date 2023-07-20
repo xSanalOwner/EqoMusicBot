4670820b80e6f37491d423333deb4a5701871bee
RahidBot <139769340+RahidBot@users.noreply.github.com>
2023-07-20 12:26:56 +0400

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "19485442"))
 API_HASH = getenv("API_HASH", "a03fcb372b3ec4e406b5d52f84b02e53")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5366398706:AAHMtMeafYqfqsvfFhZt3eBCdQsZFRGaH0k")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID", "571698989"))
@@ -18,8 +18,8 @@ START_IMG = getenv("START_IMG", "https://telegra.ph/file/ce0c5d2a22e45cf39ad59.j
 
 SESSION = getenv("SESSION", "AgAd64a2lrFnYr5PuTq1TT6kAhCefsr8zD9S8gIdO-iDCJTPUJ7DLFRo6W9pxuaUtXSd2bhjOWDxVM4V_UklsUM5WHGw28LNoKI3eYhdaeH17ho3qqCKf1bnWoBOWyVq6qsA4McIZpKmcBhVr4o8TcFD00z56yMFgW6SJBwmUYrPM_NatZag-CyBswq2assy4ebsfaQziQ7xF0h7Qh-U2hVajE1AeNr-vuJWKfb0xgStdo14LlT2zFjg_xWKD_3CwwG31OFG3WJqNfgn6Z2-dmiGHpIirnIPKhr2kVQ0qHSqMO8xJ2q-UhepOKdFpON4jaulVdpB8ToWYvi3afZ7DDRvAAAAAWV6XysA")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Cenublar")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/qruzdaa")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FriendsAilesi")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/qruzda")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5997485867").split()))
 
