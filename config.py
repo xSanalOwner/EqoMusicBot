a60d1c9504113f5963604da6a7716a0fccc35298
RahidBot <139769340+RahidBot@users.noreply.github.com>
2023-08-15 03:33:10 +0400

    update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID", "571698989"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/ce0c5d2a22e45cf39ad59.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/ce0c5d2a22e45cf39ad59.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/7d9a9179531af47fac165.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/7d9a9179531af47fac165.jpg")
 
 SESSION = getenv("SESSION", "AgAd64a2lrFnYr5PuTq1TT6kAhCefsr8zD9S8gIdO-iDCJTPUJ7DLFRo6W9pxuaUtXSd2bhjOWDxVM4V_UklsUM5WHGw28LNoKI3eYhdaeH17ho3qqCKf1bnWoBOWyVq6qsA4McIZpKmcBhVr4o8TcFD00z56yMFgW6SJBwmUYrPM_NatZag-CyBswq2assy4ebsfaQziQ7xF0h7Qh-U2hVajE1AeNr-vuJWKfb0xgStdo14LlT2zFjg_xWKD_3CwwG31OFG3WJqNfgn6Z2-dmiGHpIirnIPKhr2kVQ0qHSqMO8xJ2q-UhepOKdFpON4jaulVdpB8ToWYvi3afZ7DDRvAAAAAWV6XysA")
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/qruzda")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5997485867").split()))
 
 
-FAILED = "https://telegra.ph/file/ce0c5d2a22e45cf39ad59.jpg"
+FAILED = "https://telegra.ph/file/7d9a9179531af47fac165.jpg"
