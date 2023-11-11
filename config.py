484c11439d1b4612dc18f66479b892b1951d8683
Theferid <122831032+Theferid@users.noreply.github.com>
2023-11-12 00:36:44 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID", "26090016"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/ea120c0df12d20f9d9e4f.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/ea120c0df12d20f9d9e4f.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/6e111cd5542ba1ae0d731.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/6e111cd5542ba1ae0d731.jpg")
 
 SESSION = getenv("SESSION","AgCy7T0ucAzfrLR8V-4UG0agpZ-z9Jufex51s456oqbEO4mZylRm3jL68-TZBc_i32AByoqxo6Kk4T_4Wh-RrW-6EYMp--XOvq261cEprESQhdGztSbNZnZq4QNLXVF80zBUbj7SX0SxnjBMZ7nEq4XBrrlVg9gs-3e3FDjkLdPWIUjnsbbxOdQpeIxEU45tb05jY_x0D5RsZWH0G7bqb0tvvx5_dzkkDysW5nQwr0lNPxmrEBKOUm562Niyw0xnnub0nwXcUHDGC9VLdwxmVKhUnupaTdVw4FULfefEJ_Jn9TLkL8u7-agkLMF8pdpyA7wBqH0s7p9Gt7OrxjZg7wd3AAAAAZGxhY0A")
 
@@ -24,4 +24,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/feroobots")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "26090016").split()))
 
 
-FAILED = "https://telegra.ph/file/ea120c0df12d20f9d9e4f.jpg"
+FAILED = "https://telegra.ph/file/6e111cd5542ba1ae0d731.jpg"
