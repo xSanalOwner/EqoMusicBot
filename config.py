866f95398c9dc133fdbd4b6eb1349440004e6a8e
HesenovHuseyn <87847004+Hesenovhuseyn@users.noreply.github.com>
2023-11-19 11:30:01 +0400

    update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID", "6409880504"))
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/b7e66d35f9e3cc7f22771.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/b7e66d35f9e3cc7f22771.jpg")
 
-SESSION = getenv("SESSION", "AgAjRm7-F8o-ZPMmrNBw5kunmu33epAYSGiDjaiDLi8csbpRxaGZZ3p9ZIlf3ub52t1XhYfUt4IEBfG7X6PiBzIllswd6t0qywIgqhsNhlLiwH6o2qBsg4mMQU1upvEKvZ_9FXfI0ScQnlGarXUqiuldk0mnGTWCn9aa_IAsOtGoHQvwTAzoznpQrcPe3h0HDyC725SwxkOpVGZbxagM3XnBmNv4mWZj_d0alEmC9a1iFI_Ssimsl6npmvf5Yia3hbs5C3wFNSmK0ddXsi32qboFo89srYk8VhcP5vs_l7qwiILwjJxS1AjRe01Dq_rUuThVpvUrOueA4yg3aqnkXJ1GAAAAAYt_8UUA")
+SESSION = getenv("SESSION", "AgCInfoADronaLRqK7IYy02x5RxWt5u6I5nWH6AAvNVqLv1mZ0jviPKk7zi9citza8dyvsB7KRHDdNxSiRrSw00UUQWxeM9pm3WTJYayFJ8OOCwSvZqb-Z09CblsrA7G7qONuOpFkBwEJeNjo5a-fQjsvd69YMKI6aoVXlpOPsSMZ59TcpPe9ja3zHeLY4R-ALlpTr_uPQc2vwNpPBxpp2rq315gU90frHElcHjUHyM8qH7y5dZMWlDYve-MM2HtOdMTftTShSEfKOpkhXcYj7IP-Zq5lGgyiyUAYFTW2tI2J1GfXi6d3XX2Wl-oGMsMVE9TSUYo-u6cJVD8o41adaWI_Epx0wAAAAGLf_FFAA")
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/OliqarxTeam")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RelaxBlogs")
