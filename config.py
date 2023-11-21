3d1fd5d039a47809dd0203fe6307b3ece89c30c1
ElikoAndMee <legendmuzkao99@gmail.com>
2023-11-21 17:19:27 +0400

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID", "6513170849"))
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
 
-SESSION = getenv("SESSION","AgAF7JiTaEs5IX_VxQwF9vHwO02iLoJuLfhnGabS-E5foHVsc7gSAUuA7YyJnN4AQsZgsZCI-2sMZ8e5ccSDddyuKuPbKSh1-9csNT6KIPyhaZ6BAboW0oMdd6_uWAGtnhimZOuPd8_PWl-CG_Vhb5N6lSsnt__PFcqc549NABxtQ4UuxukI8-s-8oMSbn-pw3OSmVs0-d4UDxQdZv9fGakBI5O3Jdfex_gCvY8M4r-vsqjCNjkOzaP5k6UlgDkIyjwcmLuSiA9RhLeMDH_O2Ij6dUoKuUmHRo-wP67GIIbXbvKpFCvj41tRDr7aHFc0wn87ouJsaDYfJH-xC1JQTsTBAAAAAZ5TQW8A")
+SESSION = getenv("SESSION","AgAIO2hRJuQVbYBHK-zNY9x17pV1LNQobfd4Q0d41P0zRs93369AKMaPYXPaGwXzmBotkURYFOA5U2M17KH5VmGL1zwiQlW4_QVIjf0a_2EWh5kx8h87mBg5tvfSAfqGzlXOAxguU16z1DatyuNutInW0O6qPiZrZCr13Et-4o0kDapbhsjiFSjfSe1ixspDfvVY_Ym3Qwene8hPzAowffJleJzzAhD0-qwX0VDcR0xN6Hz1WQAUuGXSVMMCsG2XWcR0NmEC0zbsDrIogMtmPGPY6RXFLjBy6nq6TepVx5lHdfsdmE4qR-h0TKr-bYINr0-34c5Ti4Scualc_PzEdCxVAAAAAZ5TQW8A")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/OliqarxTeam")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ElikoResmi")
 
