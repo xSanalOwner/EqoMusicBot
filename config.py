12f211fe170678f2991a1900097f461138f3e5a8
ElikoAndMee <legendmuzkao99@gmail.com>
2023-11-21 17:05:32 +0400

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID", "6513170849"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/b7e66d35f9e3cc7f22771.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/b7e66d35f9e3cc7f22771.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
 
 SESSION = getenv("SESSION","AgAF7JiTaEs5IX_VxQwF9vHwO02iLoJuLfhnGabS-E5foHVsc7gSAUuA7YyJnN4AQsZgsZCI-2sMZ8e5ccSDddyuKuPbKSh1-9csNT6KIPyhaZ6BAboW0oMdd6_uWAGtnhimZOuPd8_PWl-CG_Vhb5N6lSsnt__PFcqc549NABxtQ4UuxukI8-s-8oMSbn-pw3OSmVs0-d4UDxQdZv9fGakBI5O3Jdfex_gCvY8M4r-vsqjCNjkOzaP5k6UlgDkIyjwcmLuSiA9RhLeMDH_O2Ij6dUoKuUmHRo-wP67GIIbXbvKpFCvj41tRDr7aHFc0wn87ouJsaDYfJH-xC1JQTsTBAAAAAZ5TQW8A")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/OliqarxTeam")
@@ -23,4 +23,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ElikoResmi")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5615367098").split()))
 
 
-FAILED = "https://telegra.ph/file/b7e66d35f9e3cc7f22771.jpg"
+FAILED = "https://telegra.ph/file/1b08429e371b92a1fc483.jpg"
