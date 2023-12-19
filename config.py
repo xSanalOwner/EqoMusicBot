08e45620408c593e0001cbfa1f74b687ca485b2e
Theferid <122831032+Theferid@users.noreply.github.com>
2023-12-20 01:04:46 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", "24852083"))
-API_HASH = getenv("API_HASH", "02dd65f0bae35e92f1692af0302c0c18")
+API_ID = int(getenv("API_ID", "28565438"))
+API_HASH = getenv("API_HASH", "a38714503f1573651bb0ab1ab8f5b1a1")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6875746501:AAG9-S7mdLH6LZ9U14lA8s7-WizPacUgbxU")
+BOT_TOKEN = getenv("BOT_TOKEN", "6843376045:AAHkLQpKxKxttqTXloXLTRYqH-2_2PjmTy0")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID", "6859815593"))
+OWNER_ID = int(getenv("OWNER_ID", "6972789052"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/37604c5f3a8eed43ba846.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/37604c5f3a8eed43ba846.jpg")
 
-SESSION = getenv("SESSION","AgBf58J80YA8STiDzbrKXo4kgr8JV9TSXpUyDkNxSxkNAMAff8vSQy5XXhDBB70YYxXkoc9xr4AiUCyWqtf2Pr8UHfnaYJiMsge9OepPgbaHxp7B2jzglzqxS04wTkXSoHBuw7Kdig-VUrBMs8l56WUj6KupgO3dtGOYbzENBz7NNuH19j9XrEsjGxlv8-cVaqtLLvwymvJvcvAfLhKq_uCyXFn54ekoFa1PtSqaTSdoHfCdmClNYjiPzW2rm9vOwi51WGI7wn2CPJBPg-C5AsdNFs-RvMsMSVq_9xTuEMm1XffiYCG6snskkc2gNgiMx419QnQ-UatqNo01Iigv9RYTAAAAAZskFyEA")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/OliqarxTeam")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PasterXmusic")
+SESSION = getenv("SESSION","AgC6FgpFLKqb5KQVTC4u3oEKlJcAOwThKq81I-s67KQ-0G1bH212pysZzYPfLfAWvI1wmRFGSqBVam3ZNbQ1CTaDfMAUNwJvAsRSETLOkc45pIts6DDf5MgvKOb4Y4ZPvAFykC4PAyNDIqBrzoEnii76V04-RTDWtuJdEw-BRpnGE9LKYmSskiDzDxcoaMMEJCzEoczBzaRg6s3hv5KXk33eOViybQChnIukqLBSZOOjg7tkkyryy1MdP4Xkiiu9Rza7-gMetWDmoSDaxXepv06jc-ddrQ8Q6cTKVN5Ejkx_CZtGW6A0EyFG2MVWhn8yWJR_nOTcbJUsrVJTp5rmKDg5AAAAAU8ab7EA")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sah_team")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FerooBots")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5615367098").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6972789052").split()))
 
 
-FAILED = "https://telegra.ph/file/1b08429e371b92a1fc483.jpg"
+FAILED = "https://telegra.ph/file/37604c5f3a8eed43ba846.jpg"
