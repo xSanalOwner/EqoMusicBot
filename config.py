7c3435295e0ac8e7f63d5d820cbef46c5ca61d40
ElikoAndMee <legendmuzkao99@gmail.com>
2023-12-19 21:15:33 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", "29171757"))
-API_HASH = getenv("API_HASH", "f2a2d9619b545d116fc42b5fee5d592e")
+API_ID = int(getenv("API_ID", "24852083"))
+API_HASH = getenv("API_HASH", "02dd65f0bae35e92f1692af0302c0c18")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "6875746501:AAG9-S7mdLH6LZ9U14lA8s7-WizPacUgbxU")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID", "6859815593"))
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
 
-SESSION = getenv("SESSION","AgCyMAZ7yN4DLI01NwLIDu6nMVBPlUV_EoUuKpMmn7CiCjWtgVqosjxbZNiEA27kzpLr3wZgGMXvyLuNKZlVDHNAHBN4O8SShGsucQP9WKI_71prJHUU4xcsNWrl3ZOU-SrbiKcuwl1--JjrlJtjtS-GvoqJlvZayxy9-jMSLzco8V2H5Z8oWYxtq5pXhDrfdiHnNrS3sRJ38dFc-JsatcLrmAt3ebxGLLi6UQwdPXTaaVR-qdigAWxMFKQiZk8GLfFGjqgjkOq3s9EJH-MNX6AX9Gla7Yl--f4nCVS0h44b6_G6Er8xb6jHSu6kTrl1Jb8795uBWOiqC3X0mf8luCbuAAAAAYclCgAA")
+SESSION = getenv("SESSION","AgBf58J80YA8STiDzbrKXo4kgr8JV9TSXpUyDkNxSxkNAMAff8vSQy5XXhDBB70YYxXkoc9xr4AiUCyWqtf2Pr8UHfnaYJiMsge9OepPgbaHxp7B2jzglzqxS04wTkXSoHBuw7Kdig-VUrBMs8l56WUj6KupgO3dtGOYbzENBz7NNuH19j9XrEsjGxlv8-cVaqtLLvwymvJvcvAfLhKq_uCyXFn54ekoFa1PtSqaTSdoHfCdmClNYjiPzW2rm9vOwi51WGI7wn2CPJBPg-C5AsdNFs-RvMsMSVq_9xTuEMm1XffiYCG6snskkc2gNgiMx419QnQ-UatqNo01Iigv9RYTAAAAAZskFyEA")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/OliqarxTeam")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PasterXmusic")
 
