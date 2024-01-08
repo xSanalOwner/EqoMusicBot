c217212c502d8515f5f646501966057d8575d271
ElikoAndMee <legendmuzkao99@gmail.com>
2024-01-08 10:25:19 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", "24852083"))
-API_HASH = getenv("API_HASH", "02dd65f0bae35e92f1692af0302c0c18")
+API_ID = int(getenv("API_ID", "20272501"))
+API_HASH = getenv("API_HASH", "75dee2d6b24b0ed686ab19b42efa7a32")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "6875746501:AAEUwzo5JFdQr75Oll6e8E4MmeBG1-uGMak-2_2PjmTy0")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID", "6859815593"))
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/1b08429e371b92a1fc483.jpg")
 
-SESSION = getenv("SESSION","AgAeXOK34NLyG54hSV_hMaoXJ2XC-0AJ03FCtD5R_ywVZe93kjnL-trL9wNwuLsRkq9eLZMke0WY_-SzLZX2sJnJbuA5GLLKAUeXHeHbZfIU89gGGeMvfD92MQITc61y9v2s1kp7do2lLxllbTtePqe2VyowZNdoW1wPtT5ts7Q9gxnU0w7Lkmg8L_Kny5kQqwGRDyyTA0mx1MqiMM9MvnbgNIQw-GSDKDTyXRUFaenPx9dbqpUAg7mhubQmnD4QNgvszghg14omfSjCUsFjZxI30BCBD5J4Q6dEBcD6ox7NzWRlaIVC66l7uLmdSF3hUl41Jc9Rm8lisAPLi-MLxb44AAAAAZskFyEA")
+SESSION = getenv("SESSION","AgCWkXMzNaK69ZKWc_AOHSee_3HMJbWALGEGA2yvE-oP0ga17FVjl2M8wCsldK8r5IYvJoyNJcXVpA75Fm33Gqi9MwwNGt9BxL8PmIizo-tfAtpTBA-GyZKkI6SPAARtaEmaZFYY_LRePM83bRveeIj4UKEOxt13apHs-zudr-mR3CDyDZ6V9AbotZmg5e6rbw0kJsGk5DrliHbXsUQbDwisq1FAxyxZBA4I02v21_UcJhEmF5oRSpoTePOhzDNy7LmoUPFBItQInEZpbdbRhC5eMQkW5_36MBpbBky93bNW1mMK1Va6hXgfkYhjgNz84dUrI0-MBHhy0Y0el-L9y6bIAAAAAZGueh4A")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KarabakhSoldiers")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PasterXmusic")
 
