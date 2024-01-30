a1f39f90b27e0321fc179ce8c203577a0e04b826
Elgün Abbasovh <legendmuzkao99@gmail.com>
2024-01-30 16:44:30 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", "25540216"))
-API_HASH = getenv("API_HASH", "08d15f61e57431d7f5711ac8567e5937")
+API_ID = int(getenv("API_ID", "28167952"))
+API_HASH = getenv("API_HASH", "27618e15dd9a461849bbe5800413346f")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6370639090:AAEY8mVE4QdtlcPrwSm3dU04YzcJRAg5iiE-uGMak")
+BOT_TOKEN = getenv("BOT_TOKEN", "6370639090:AAGc3790U0o-19aMpnJUJbgDSmBCTnxkBa0")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID", "5567060261"))
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID", "5567060261"))
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/219642edbf3d6634e4a5e.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/219642edbf3d6634e4a5e.jpg")
 
-SESSION = getenv("SESSION","AQBh4QP_PAbxoo5nTrT5yvvD9KkVv2iGo_c0xSQ-iRxTt9qAXH9h80g77zdvrf13xH7teNDDfT8PnsMsUZG6W5yHxF9XWLeelRjDJ76UO2pcri6fkHZwHoPQIR6fCeRR5iM8zNGtSF27jEpYD7j0DZmEA2XQFNct2lHE856HK4-iZO4xi1X8sZSW6oRv1pG3ZX6AxFqDn7rqOaeH2osk7r3EVoA5RjTqW03PNWLDsUnHgeplC7qrbcBzdZrpk-OFuJbiyoJ6HrFsZO6UsbOCti0HVDncXBi3AVTyrVl_w0PQ5p2dj0NNTjR64QeThdHST31-BNZlwHHXt57CLhtDeoAAAAATBROT4A")
+SESSION = getenv("SESSION","AgADSTwbjnfTScnqVohIzWV9zHZsebypnlH8QmQx1vqlMWe6tTEWPA3ZusqHMswVKGFF_iWxT41nmgCrzgPndujQwwRAR9a6J2VWX9CvBZ2S8-XDyRT8NZjuNVGR6yc3JXgPZ24K5W6rmXjXstnfFWsDqYs72eO8PfLlPNlyQEgAheucfYAddzEsFbP7yq2JjYr8FdwjQbbnND-pOBwMSvAzn28ZdJAwfjNXrroRGsT5_NKcCV1nDOBYtmqnRvCSDMD7cepFHa0rkPb4VBszdQ3Tr1QOhk5EhrNCAUoP37osJCMjo0uCsSwTolO8eNqzjyPS9G3XOGCeLeKEzS4pUiLGAAAAAXTQeH8A")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AmazingChatSS")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ssmusiclist")
 
