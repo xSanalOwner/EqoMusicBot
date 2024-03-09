69b16e79b82a85abf9314e14d99810e4bd6105f1
xSanalOwner <legendmuzkao99@gmail.com>
2024-03-09 19:03:49 +0400

    Update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -34,7 +34,7 @@ pm_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="Rəsmi Kanal 📢", url="https://t.me/MehdiBlogg"
+            text="Rəsmi Kanal 📢", url="https://t.me/RikaBots"
         ),
         InlineKeyboardButton(text="Creator 🧑‍💻", user_id=config.OWNER_ID),
     ],
@@ -54,7 +54,7 @@ gp_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="Rəsmi Kanal 📢", url="hhttps://t.me/MehdiBlogg"
+            text="Rəsmi Kanal 📢", url="hhttps://t.me/RikaBots"
         ),
         InlineKeyboardButton(text="Creator 🧑‍💻", user_id=config.OWNER_ID),
     ],
