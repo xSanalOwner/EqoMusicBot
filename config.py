3445dd43d7cbef6922abea39e37729d8834ca629
xSanalOwner <legendmuzkao99@gmail.com>
2024-08-07 22:58:18 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID", "27715768"))
-API_HASH = getenv("API_HASH", "7276a0bc7e5eafbeea78669fa0f3fd6b")
+API_ID = int(getenv("API_ID", "26150211"))
+API_HASH = getenv("API_HASH", "ece732fb4b6d28a4d7279e0705664ce2")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "6310087186:AAEByv-qT-KOsR1UzM_L6N5VoCImtRBAZiE")
+BOT_TOKEN = getenv("BOT_TOKEN", "6875746501:AAG9ejQ-GRKaXO2C6AhI9yH7bh2n2UkujmA")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID", "6944685509"))
+OWNER_ID = int(getenv("OWNER_ID", "7406471382"))
 
 PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/de3bbbf56a078bf4b5b1f.jpg")
 START_IMG = getenv("START_IMG", "https://telegra.ph/file/de3bbbf56a078bf4b5b1f.jpg")
 
-SESSION = getenv("SESSION","AgBZSDdYk7XphEuwS0ebYHjbn2iqGsNP8BK_DWZaoFR27Skq6dLJQD56llRiTGuXQ3ISExAMl9WvNFMEx8pfNDDaMSLzyP84GpwHcW65RYcvPaw9kgjg3mVbDQz9nxXd2jhVvyd1ljOKXDIEgs8lCZjWWb81tyIrSkJ11RpaMGLVl_V4BrfR1rH9fLwmKkNsnbDliG-yXCZOiUuAvnMikK7V3Js7QllTFdJy4QgniJj-83U3dedwFDD-RvrE8j37RrMV_OyYNhy5SKlDFoCStdAyxLdPd0HXb4aD4qBm7r5WVk5GAncjKVEoX8JSbJ0J7hKAXqxUu8R_NqAJdCzMVkQ-AAAAAaY_nhkA")
+SESSION = getenv("SESSION","BAGPBUMAKbGxnAY4N_Ld4limntU14zOf4XjX1fFumniYAzdlZ9BbOzMAdMU1-C8JwZCKFS08nEDMr0aO-iSOoIajJmfY5eAl-krtnKHJwdrm4jEuKNqIKsyeuBbsufibEdYQuJJZ6BO57lU9ohna18JbcLrwgJXtBrXZpFblf6ZhU_SI8faJXxsD_uY3rtOewnVnA3VD5C8nIrmMLXghPPhxJ0IvlrzLSzC4uADXnxb5t-dEcS2drGjND1pUB8PN3Jpv7lXJz3ZVL0TEwODuLJiNGTHoHIGyeTsaVQQqei5wuvSah_zS_OlVymmGe4tpnhCsuITmPNaidSoZsODUkfbvCGZRTAAAAAGS2bcGAA")
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/LyuksGroups")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RikaPlaylist")
